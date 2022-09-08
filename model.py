dbe8700d10ac8352899c090a5e27d72c3790dd77
Lana Ramsak <ramsaklana@gmail.com>
2022-09-08 22:23:42 +0200

    Dodatki na modelu

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
 import datetime
 import json
-
+from this import d
 
 DOLZINA_REZERVACIJE = datetime.timedelta(hours = 2)
 
@@ -8,9 +8,8 @@ class Stanje:
     def __init__(self, restavracija, geslo, mize={}, lokacije=[]):
         self.uporabnik = restavracija
         self.geslo = geslo
-        self.mize = sorted(mize)
+        self.mize = mize
         self.lokacije = lokacije
-        #izpis vseh rezervacij, kako?
 
     def dodaj_rezervacijo(self, rezervacija):
         datum = rezervacija.datum
@@ -18,7 +17,7 @@ class Stanje:
             if rezervacija.stevilo_oseb <= miza.stevilo_oseb:
                 if miza.timeline == {}:
                     return True
-                nov_seznam_ur = list(miza.timeline.keys()) + [datum]
+                nov_seznam_ur = miza.timeline + [datum]
                 nov_seznam_ur.sort()
                 index = nov_seznam_ur.index(datum)
                 if index == len(nov_seznam_ur) - 1:
@@ -53,8 +52,8 @@ class Stanje:
                     #kaj če je vmes program zaprt pa poteče rezervacija, zato <=
 
     def v_slovar(self):
-        miza_spomin = [miza.v_slovar() for miza in self.mize]
-        return {"restavracija" : self.restavracija, "geslo" : self.geslo, "mize" : miza_spomin, "lokacije" : self.lokacije}
+        mize_spomin = {lokacija: [miza.v_slovar() for miza in self.mize[lokacija]] for lokacija in self.mize.keys()}
+        return {"restavracija" : self.restavracija, "geslo" : self.geslo, "mize" : mize_spomin, "lokacije" : self.lokacije}
 
     def shrani_v_datoteko(self, ime_datoteke):
         with open(ime_datoteke, "w") as dat:
@@ -63,28 +62,34 @@ class Stanje:
 
     @staticmethod
     def iz_slovarja_stanje(slovar):
-        return Stanje(slovar["restavracija"], slovar["geslo"], [iz_slovarja_miza(miza) for miza in slovar["mize"]], slovar["lokacije"])
+        slovar_miz = {}
+        for lokacija in slovar["mize"].keys():
+            slovar_miz[lokacija] = []
+            for miza in slovar["mize"][lokacija]:
+                slovar_miz[lokacija].append(Miza.iz_slovarja_miza(miza))
+        return Stanje(slovar["restavracija"], slovar["geslo"], slovar_miz, slovar["lokacije"])
 
     @staticmethod
     def preberi_iz_datoteke(ime_datoteke):
         with open(ime_datoteke) as dat:
             slovar = json.load(dat)
-            return Stanje.iz_slovarja(slovar)
+            return Stanje.iz_slovarja_stanje(slovar)
 
 class Miza:
     stevilo = 0
-    def __init__(self, stevilo_oseb, lokacija, timeline={}, rezerviranost=[], zasedenost=False):
+    def __init__(self, stevilo_oseb, lokacija, timeline=[], rezerviranost=[], zasedenost=False, stevilka=0):
         self.stevilo_oseb = stevilo_oseb
         self.lokacija = lokacija
         self.timeline = timeline
         self.zasedenost = zasedenost
         self.rezerviranost = rezerviranost
+        self.stevilka = stevilka
 
     def __lt___(self, miza2):
         return self.stevilo_oseb < miza2.stevilo_oseb
 
     def rezerviraj(self, rezervacija):
-        self.timeline[rezervacija.datum] = rezervacija.datum + DOLZINA_REZERVACIJE
+        self.timeline.append(rezervacija.datum)
         self.rezerviranost.append(rezervacija)
 
     def preveri_zasedenost(self):
@@ -99,7 +104,7 @@ class Miza:
 
     def prosta(self):
         self.zasedenost = False
-        self.timeline.pop(list(self.timeline.keys())[0])
+        ##!!!!!!!!!!!self.timeline.pop(list(self.timeline.keys())[0])
 
     def naslednja_rezervacija(self):
         if self.timeline == []:
@@ -109,11 +114,11 @@ class Miza:
 
     def v_slovar(self):
         rezerviranost_spomin = [rezervacija.v_slovar() for rezervacija in self.rezerviranost]
-        return {"stevilo_oseb" : self.stevilo_oseb, "lokacija" : self.lokacija, "timeline" : self.timeline, "rezerviranost" : rezerviranost_spomin, "zasedenost" : self.zasedenost}
+        return {"stevilo_oseb" : self.stevilo_oseb, "lokacija" : self.lokacija, "timeline" : [str(time) for time in self.timeline], "rezerviranost" : rezerviranost_spomin, "zasedenost" : self.zasedenost}
 
     @staticmethod
     def iz_slovarja_miza(slovar):
-        return Miza(slovar["stevilo_oseb"], slovar["lokacija"], slovar["timeline"], [iz_slovarja_rezervacija(rezervacija) for rezervacija in slovar["rezerviranost"]], slovar["zasedenost"])
+        return Miza(slovar["stevilo_oseb"], slovar["lokacija"], [datetime.datetime.strptime(datum, "%Y-%m-%d %H:%M:%S") for datum in slovar["timeline"]], [Rezervacija.iz_slovarja_rezervacija(rezervacija) for rezervacija in slovar["rezerviranost"]], slovar["zasedenost"])
 
 
 class Rezervacija:
@@ -131,8 +136,8 @@ class Rezervacija:
         self.opravljenost = True
 
     def v_slovar(self):
-        return {"ime" : self.ime, "stevilo_oseb" : self.stevilo_oseb, "datum" : self.datum, "lokacija" : self.lokacija, "opravljenost" : self.opravljenost}
+        return {"ime" : self.ime, "stevilo_oseb" : self.stevilo_oseb, "datum" : str(self.datum), "lokacija" : self.lokacija, "opravljenost" : self.opravljenost}
     
     @staticmethod
     def iz_slovarja_rezervacija(slovar):
-        return Rezervacija(slovar["ime"], slovar["stevilo_oseb"], slovar["datum"], slovar["lokacija"], slovar["opravljenost"]) 
+        return Rezervacija(slovar["ime"], slovar["stevilo_oseb"], datetime.datetime.strptime(slovar["datum"], "%Y-%m-%d %H:%M:%S"), slovar["lokacija"], slovar["opravljenost"]) 
